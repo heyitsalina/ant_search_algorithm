8bde14033888288683675656e9ece38a39a75f18
Jonah Gräfe <jonah.greafe@gmail.com>
2023-12-07 13:06:29 +0100

    new widget for buttons

--- resources/gui.py
+++ resources/gui.py
@@ -17,7 +17,7 @@ from kivy.config import Config
 
 
 kivy.require('2.2.1')
-kivy.config.Config.set('graphics','resizable', False)
+Config.set('graphics', 'resizable', '0')
 
 
 
@@ -37,13 +37,13 @@ class GUI(App):
         Window.size = (720, 480)
         root = BoxLayout(orientation='vertical', padding=0, spacing=0)
 
-        moving_points_widget = SimulationWidget(size=(1080, 680), pos=(0, 100))
-        start_stop_button = Button(text='Start', on_press=moving_points_widget.toggle_simulation, height=100, size_hint_y=None)
+        simulation_widget = SimulationWidget(size=(1080, 680), pos=(0, 100))
+        button_widget = ButtonWidget(simulation_widget)
 
-        root.add_widget(moving_points_widget)
-        root.add_widget(start_stop_button)
+        root.add_widget(simulation_widget)
+        root.add_widget(button_widget)
 
-        # Clock.schedule_interval(moving_points_widget.update_points, 0.1)
+        # Clock.schedule_interval(simulation_widget.update_points, 0.1)
 
         return root
     
@@ -62,6 +62,22 @@ class SimulationWidget(Widget):
     def toggle_simulation(self, instance):
         self.is_running = not self.is_running
         instance.text = 'Stop' if self.is_running else 'Start'
+
+
+class ButtonWidget(BoxLayout):
+    def __init__(self, simulation_widget,**kwargs):
+        super(ButtonWidget, self).__init__(**kwargs)
+        self.simulation_widget = simulation_widget
+
+        start_stop_button = Button(text='Start', on_press=simulation_widget.toggle_simulation, height=100, size_hint_y=None)
+
+        food_button = Button(text='Food', height=100, size_hint_y=None)
+
+        colonie_button = Button(text='Colonie', height=100, size_hint_y=None)
+
+        self.add_widget(food_button)
+        self.add_widget(colonie_button)
+        self.add_widget(start_stop_button)
 
 
 if __name__ == "__main__":
