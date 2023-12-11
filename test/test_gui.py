81eada15b43d4562a1836bf044c9f2fb210ee8ee
Jonah Gräfe <jonah.greafe@gmail.com>
2023-12-11 21:27:51 +0100

    changes

--- test/test_gui.py
+++ test/test_gui.py
@@ -1,44 +1,23 @@
 import pytest
-import os
+import time
+from functools import partial
 from kivy.core.window import Window
 from kivy.clock import Clock
-from kivy.uix.button import Button
 from resources.gui import GUI, SimulationWidget, ButtonWidget
-from kivy.tests.common import GraphicUnitTest
+import unittest
 
-os.environ['KIVY_NO_FILELOG'] = '1'
 
-class TestGUI(GraphicUnitTest):
-    def test_gui_creation(self):
-        # Initialize the Kivy application
-        app = GUI()
-        app.run()
 
-    def test_app_builds_without_errors(self):
-        # Test that the application builds without errors
-        app = GUI()
-        app.initialize_kivy()
-        app.build()
+class TestGUI(unittest.TestCase):
+    def pause(*args):
+        time.sleep(0.000001)
 
-    def test_simulation_widget_generate(self):
-        # Test the generate method in SimulationWidget
-        widget = SimulationWidget()
-        widget.generate()
-        assert widget.array is not None
-        assert widget.points is not None
+    def run_test(self, app, *args):
+        Clock.schedule_interval(self.pause, 0.000001)
 
-    def test_simulation_widget_toggle_simulation(self):
-        # Test the toggle_simulation method in SimulationWidget
-        widget = SimulationWidget()
-        initial_state = widget.is_running
-        widget.toggle_simulation(Button(text='Start'))
-        assert widget.is_running != initial_state
-
-    def test_button_widget_change_window_size(self):
-        # Test the change_window_size method in ButtonWidget
         widget = ButtonWidget(SimulationWidget())
         initial_size = Window.size
-        widget.change_window_size((800, 600))
+        widget.change_window_size((Window.size[0] * 2, Window.size[1] * 2))
 
         # Schedule a function to check the assertion after a short delay
         def check(dt):
@@ -49,10 +28,13 @@ class TestGUI(GraphicUnitTest):
         # Run the Kivy main loop for a short duration to allow events to be processed
         Clock.tick()
 
-    def test_clock_schedule_interval(self):
-        # Test the Clock.schedule_interval with a lambda function
-        widget = SimulationWidget()
-        Clock.schedule_interval(lambda dt: widget.update_world(dt), 0.1)
+        app.stop()
+
+    def test_create_gui(self):
+        app = GUI()
+        p = partial(self.run_test, app)
+        Clock.schedule_once(p, 0.000001) 
+        app.run()
 
 
 if __name__ == "__main__":
