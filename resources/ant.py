727317d3f30367b085a302bb4c580c93d92ed3d9
Anas <anas.alhariri@study.hs-duesseldorf.de>
2023-12-18 22:58:19 +0100

    added some comments

--- resources/ant.py
+++ resources/ant.py
@@ -47,26 +47,32 @@ class Ant:
         position = np.array(self.coordinates)
         
         if self.epoch == 0:
+            #Randomly set initial direction in coordinates
             theta = np.random.uniform(0, 2 * np.pi)
             z = np.random.uniform(-1, 1)
             
             x = np.sqrt(1 - z**2) * np.cos(theta)
             y = np.sqrt(1 - z**2) * np.sin(theta)
             
+            #Set direction vector        
             self.direction = np.array([x, y])
 
+            #Update position using the direction vector
             position += self.direction
             self.coordinates = tuple(position)
 
             self.epoch += 1
         else:
+            #Apply a random rotation to the existing direction
             angle_offset = np.random.uniform(-np.pi/4, np.pi/4)
             rotation_matrix = np.array([[np.cos(angle_offset), -np.sin(angle_offset)],
                                     [np.sin(angle_offset), np.cos(angle_offset)]])
             
+            #Rotate and normalize the direction vector
             self.direction = np.dot(rotation_matrix, self.direction)
             self.direction = self.direction / np.linalg.norm(self.direction)
             
+            #Update position using the rotated direction
             position += self.direction
             self.coordinates = tuple(position)
 
