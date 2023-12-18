96519de703502bd274383bc743ce0dadb1ec218e
Anas <anas.alhariri@study.hs-duesseldorf.de>
2023-12-18 19:12:34 +0100

    Updated docstring

--- resources/ant.py
+++ resources/ant.py
@@ -8,8 +8,8 @@ class Ant:
         Args:
         pheromone_status (float): 
             The current level of pheromone detected by the ant.
-        coordinates (list):
-            A list of all visited coordinates.
+        start_coordinates (tuple):
+            The initial coordinates representing the starting position of an ant.
         angle (float):
             The current angle of the ant in the search space.
         size (float):
