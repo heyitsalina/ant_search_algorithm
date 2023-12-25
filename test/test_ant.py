eaac772c3694490cd7ec0d183e07f825dc99f93f
Anas <anas.alhariri@study.hs-duesseldorf.de>
2023-12-25 04:19:39 +0100

    test if ant inside radius

--- test/test_ant.py
+++ test/test_ant.py
@@ -1,6 +1,9 @@
 import pytest
-from resources.ant import *
+from resources.ant import Ant
 
+ant = Ant(pheromone_status=1,
+              coordinates=(100.0, 100.0),
+              amount_to_carry=20)
 
 def test_switch_pheromon():
     pass
@@ -9,7 +12,15 @@ def test_move():
     pass
 
 def test_find_food():
-    pass
+    ant.coordinates = (145, 145)  # Inside radius
+    result = ant.find_food((100, 100))
+    assert result is not None, "Should return coordinates when ant is inside radius"
+
 
 def test_carry_food():
     pass
+
+
+if __name__ == "__main__":
+    test_move()
+    
