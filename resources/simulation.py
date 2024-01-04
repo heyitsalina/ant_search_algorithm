8dce6257240b5f6061df3b856f1e99d3e6350ea0
honnigmelone <nikita.rosik@study.hs-duesseldorf.de>
2024-01-05 00:47:44 +0100

    added possible changes in simulation

--- resources/simulation.py
+++ resources/simulation.py
@@ -49,6 +49,12 @@ class Simulation:
     def next_epoch(self):
         for colony in self.colonies:
             for ant in colony.ants:
+                #for food in self.food:
+                 #   if ant.try_carry_food(food):
+                  #      ant.carry_food(food)
+                   # if ant.drop_food(food):
+                    #    ant.drop_food(food)
+
                 self.update_pheromone(ant)
                 future_position = ant.move()
                 adjusted_position = self.check_future_position(future_position)
