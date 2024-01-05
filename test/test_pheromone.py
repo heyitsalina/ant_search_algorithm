b284addd4742f4035904c57e37790e1c8743da5b
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-01-05 23:14:03 +0100

    test_leave_pheromone()

--- test/test_pheromone.py
+++ test/test_pheromone.py
@@ -10,3 +10,12 @@ def test_initialization():
     assert pheromone.pheromones.shape == (2, grid_shape[1], grid_shape[0])
     assert np.all(pheromone.pheromones == 0)
     assert np.all(pheromone.timestamps == 0)
+    
+def test_leave_pheromone():
+    grid_shape = (10, 10)
+    pheromone = Pheromone(grid_shape)
+    pos = (5, 5)
+    pheromone_status = 1
+
+    pheromone.leave_pheromone(pos, pheromone_status)
+    assert pheromone.pheromones[1, pos[1], pos[0]] == pheromone_status
