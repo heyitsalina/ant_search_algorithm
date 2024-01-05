ee08179108e3414d66fded0311880e85d6cc05f7
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-01-05 18:54:17 +0100

    added docstring to the class pheromone

--- resources/pheromone.py
+++ resources/pheromone.py
@@ -1,14 +1,35 @@
 import numpy as np
 
 class pheromone:
-    def __init__(self, pheromone_shape):
-        self.pheromones = np.zeros((pheromone_shape[0], pheromone_shape[1], 2))
+    def __init__(self, grid_shape):
+        """
+        Manages pheromone information in a tensor within a simulated environment.
+        The tensor represents the pheromone strength at different positions within the simulation area.
 
-    def put_pheromone(self):
+        Args:
+            grid_shape (Tuple[int, int]): A tuple representing the height and width of the simulated environment.
+
+        Attributes:
+            pheromones (numpy.ndarray): A 3D numpy array of dimensions (Height, Width, Depth), storing the pheromone strength at each visited position(int).
+                                        The depth represents different pheromone matrices ('coming from colony' = -1 | 'coming from food' = 1).
+
+        Methods:
+            leave_pheromone():
+                Leaves a pheromone from each ant at every movement, adding it to the corresponding location in the tensor.
+
+            get_pheromone_level():
+                Determines the pheromone level at a specific location in the tensor.
+
+            reduce_pheromone(reducing_factor: float, timeframe: float):
+                Reduces the pheromone strength in the tensor after a certain timeframe by a specific factor.
+        """
+        self.pheromones = np.zeros((grid_shape[0], grid_shape[1], 2))
+
+    def leave_pheromone(self):
         pass
     
     def get_pheromone_level(self):
         pass
 
-    def reduce_pheromone(self, reducing_factor, time_frame):
+    def reduce_pheromone(self, reducing_factor, timeframe):
         pass
