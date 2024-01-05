e0dce4f5fd53f9b1abe99857fd3e1a2e12649820
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-01-05 23:12:38 +0100

    test_initiallization()

--- test/test_pheromone.py
+++ test/test_pheromone.py
@@ -1,4 +1,12 @@
 import pytest
+import numpy as np
 from resources.pheromone import Pheromone
 
-pheromone = Pheromone(grid_shape = (1920, 1080))
+
+def test_initialization():
+    grid_shape = (10, 10)
+    pheromone = Pheromone(grid_shape)
+
+    assert pheromone.pheromones.shape == (2, grid_shape[1], grid_shape[0])
+    assert np.all(pheromone.pheromones == 0)
+    assert np.all(pheromone.timestamps == 0)
