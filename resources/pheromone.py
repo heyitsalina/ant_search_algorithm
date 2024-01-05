860fa3d232749450b8d6a9d7ba74527b24c92ccc
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-01-05 17:58:10 +0100

    added put_pheromone()

--- resources/pheromone.py
+++ resources/pheromone.py
@@ -3,3 +3,7 @@ import numpy as np
 class pheromone:
     def __init__(self, pheromone_shape):
         self.pheromones = np.zeros((pheromone_shape[0], pheromone_shape[1], 2))
+
+    def put_pheromone(self):
+        pass
+    
