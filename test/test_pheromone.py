6c507d9687345f797571666f3a0c7baaf8626cc6
Tim S. <timsan99@gmx.de>
2024-01-18 18:53:00 +0100

    Created a test-method for the pheromone-reduction

--- test/test_pheromone.py
+++ test/test_pheromone.py
@@ -30,3 +30,35 @@ def test_get_pheromone_level():
     levels = pheromone.get_pheromone_level(pos)
 
     assert levels == {'coming from colony': 0, 'coming from food': pheromone_status}
+
+def test_reduce_pheromones():
+    grid_shape = (10, 10)
+    pheromone = Pheromone(grid_shape)
+    
+    pos = (5, 5)
+
+    # Ant coming from food
+    pheromone_status = 1
+    pheromone.leave_pheromone(pos, pheromone_status)
+
+    # Ant coming from colony
+    pheromone_status = - 1
+    pheromone.leave_pheromone(pos, pheromone_status)
+
+    # First reduction
+    pheromone.reduce_pheromones(reducing_factor = 0.5)
+    levels = pheromone.get_pheromone_level(pos)
+    assert levels == {'coming from colony': - 0.5, 'coming from food': 0.5}
+
+    # Second reduction
+    pheromone.reduce_pheromones(reducing_factor = 0.5)
+    levels = pheromone.get_pheromone_level(pos)
+    assert levels == {'coming from colony': - 0.25, 'coming from food': 0.25}
+
+    # Further reductions
+    for _ in range(5):
+        pheromone.reduce_pheromones(reducing_factor = 0.5)
+    levels = pheromone.get_pheromone_level(pos)
+    assert levels == {'coming from colony': 0, 'coming from food': 0}
+
+    
