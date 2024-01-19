df96f4b9c5ad19722e7f1b93c0fcafd791f9d64a
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-01-19 12:32:03 +0100

    test_pheromone: fix typo

--- test/test_pheromone.py
+++ test/test_pheromone.py
@@ -7,8 +7,8 @@ def test_initialization():
     grid_shape = (10, 10)
     pheromone = Pheromone(grid_shape)
 
-    assert pheromone.pheromones.shape == (2, grid_shape[1], grid_shape[0])
-    assert np.all(pheromone.pheromones == 0)
+    assert pheromone.pheromone_array.shape == (2, grid_shape[1], grid_shape[0])
+    assert np.all(pheromone.pheromone_array == 0)
     
 def test_leave_pheromone():
     grid_shape = (10, 10)
@@ -17,7 +17,7 @@ def test_leave_pheromone():
     pheromone_status = 1
 
     pheromone.leave_pheromone(pos, pheromone_status)
-    assert pheromone.pheromones[1, pos[1], pos[0]] == pheromone_status
+    assert pheromone.pheromone_array[1, pos[1], pos[0]] == pheromone_status
     
 def test_get_pheromone_level():
     grid_shape = (10, 10)
