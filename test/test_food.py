d6af7e6bfe001112e23798af18d25b9c6fd61972
honnigmelone <nikita.rosik@study.hs-duesseldorf.de>
2024-02-05 13:39:36 +0100

    Added basic food test

--- test/test_food.py
+++ test/test_food.py
@@ -1,2 +1,12 @@
 import pytest
 from resources.food import *
+
+
+def test_food_initialization():
+    food = Food(size=10, coordinates=(100,100), amount_of_food=100, show_life_bar= True)
+
+    assert food.size == 10 
+    assert food.coordinates == (100, 100)
+    assert food.amount_of_food == 100
+    assert food.show_life_bar == True
+
