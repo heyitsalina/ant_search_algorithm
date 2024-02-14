d53a97feb534cd610a42b1c674d688232e4e8e02
Jonah Gräfe <jonah.greafe@gmail.com>
2024-02-14 21:00:03 +0100

    added overview plot

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -10,6 +10,8 @@ def build_pdf():
     latex_template = """
     \\documentclass{article}
     \\usepackage{hyperref}
+    \\usepackage{tikz}
+    \\usepackage{pgfplots}
     \\begin{document}
 
     \\title{Ant Search Statistics}
@@ -27,6 +29,24 @@ def build_pdf():
     \\section{Food}
 
     %s
+
+    \\section{Overview}
+
+    \\begin{tikzpicture}
+    \\begin{axis}[%s, width=12cm, height=7cm]
+
+    \\addplot[only marks, mark=square, mark size=3pt, blue] coordinates {
+    %s
+    };
+    \\addlegendentry{Colony}
+
+    \\addplot[only marks, mark=*, mark size=3pt, red] coordinates {
+    %s
+    };
+    \\addlegendentry{Food}
+
+    \\end{axis}
+    \\end{tikzpicture}
 
     \\end{document}
     """
@@ -57,16 +77,27 @@ def build_pdf():
             food_content += "\\item Coordinates: %s\n" % food_data['coordinates']
             food_content += "\\end{itemize}\n"
 
-    latex_document = latex_template % (simulation_content, colonies_content, food_content)
+    min_x, max_x, min_y, max_y = data["simulation"][0]["boundaries"]
+    plot_range = f"xmin={min_x}, xmax={max_x}, ymin={min_y}, ymax={max_y}"
+
+    plot_colony = ""
+    plot_food = ""
+    
+    for colony_data in data['colonies']:
+        x, y = colony_data['coordinates']
+        plot_colony += f"({x+50},{y+50}) "
+
+    for food_data in data['food']:
+        x, y = food_data['coordinates']
+        plot_food += f"({x+50},{y+50}) "
+
+    latex_document = latex_template % (simulation_content, colonies_content, food_content, plot_range, plot_colony, plot_food)
 
     directory = time.strftime("%Y-%m-%d_%H-%M-%S")
-
     path = os.path.join("statistics", directory)
     os.makedirs(path, exist_ok=True)
-
     with open(os.path.join(path, "statistics.tex"), 'w') as output_file:
         output_file.write(latex_document)
 
-    os.chdir("statistics\\" + directory)
-
+    os.chdir(path)
     os.system(f"pdflatex statistics.tex")
