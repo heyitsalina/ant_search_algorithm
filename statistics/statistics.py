a724776b04ee2b8c61a122eb42001bfec3984c75
Jonah Gräfe <jonah.greafe@gmail.com>
2024-02-14 13:02:35 +0100

    minor changes

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -2,61 +2,63 @@ import json
 import os
 import time
 
-with open('statistics/statistics.json', 'r') as json_file:
-    data = json.load(json_file)
 
-latex_template = """
-\\documentclass{article}
-\\usepackage{hyperref}
-\\begin{document}
+def build_pdf():
+    with open('statistics/statistics.json', 'r') as json_file:
+        data = json.load(json_file)
 
-\\title{Ant Search Statistics}
-\\author{\\url{https://github.com/heyitsalina/ant_search_algorithm}}
-\\maketitle
+    latex_template = """
+    \\documentclass{article}
+    \\usepackage{hyperref}
+    \\begin{document}
 
-\\section{Colonies}
+    \\title{Ant Search Statistics}
+    \\author{\\url{https://github.com/heyitsalina/ant_search_algorithm}}
+    \\maketitle
 
-%s
+    \\section{Colonies}
 
-\\section{Food}
+    %s
 
-%s
+    \\section{Food}
 
-\\end{document}
-"""
+    %s
 
-colonies_content = ""
-if isinstance(data['colonies'], list):
-    for idx, colony_data in enumerate(data['colonies']):
-        colonies_content += "\\subsection*{Colony %d}\n" % (idx + 1)
-        colonies_content += "\\begin{itemize}\n"
-        colonies_content += "\\item Amount: %s\n" % colony_data['amount']
-        colonies_content += "\\item Coordinates: %s\n" % colony_data['coordinates']
-        colonies_content += "\\item Pheromone Grid: %s\n" % colony_data['pheromone grid']
-        colonies_content += "\\item Color: %s\n" % colony_data['color']
-        colonies_content += "\\item Food Count: %s\n" % colony_data['food counter']
-        colonies_content += "\\end{itemize}\n"
+    \\end{document}
+    """
 
-food_content = ""
-if isinstance(data['food'], list):
-    for idx, food_data in enumerate(data['food']):
-        food_content += "\\subsection*{Food %d}\n" % (idx + 1)
-        food_content += "\\begin{itemize}\n"
-        food_content += "\\item Start amount: %s\n" % food_data['start amount']
-        food_content += "\\item Amount of food: %s\n" % food_data['amount of food']
-        food_content += "\\item Coordinates: %s\n" % food_data['coordinates']
-        food_content += "\\end{itemize}\n"
+    colonies_content = ""
+    if isinstance(data['colonies'], list):
+        for idx, colony_data in enumerate(data['colonies']):
+            colonies_content += "\\subsection*{Colony %d}\n" % (idx + 1)
+            colonies_content += "\\begin{itemize}\n"
+            colonies_content += "\\item Amount: %s\n" % colony_data['amount']
+            colonies_content += "\\item Coordinates: %s\n" % colony_data['coordinates']
+            colonies_content += "\\item Pheromone Grid: %s\n" % colony_data['pheromone grid']
+            colonies_content += "\\item Color: %s\n" % colony_data['color']
+            colonies_content += "\\item Food Count: %s\n" % colony_data['food counter']
+            colonies_content += "\\end{itemize}\n"
 
-latex_document = latex_template % (colonies_content, food_content)
+    food_content = ""
+    if isinstance(data['food'], list):
+        for idx, food_data in enumerate(data['food']):
+            food_content += "\\subsection*{Food %d}\n" % (idx + 1)
+            food_content += "\\begin{itemize}\n"
+            food_content += "\\item Start amount: %s\n" % food_data['start amount']
+            food_content += "\\item Amount of food: %s\n" % food_data['amount of food']
+            food_content += "\\item Coordinates: %s\n" % food_data['coordinates']
+            food_content += "\\end{itemize}\n"
 
-directory = time.strftime("%Y-%m-%d_%H-%M-%S")
+    latex_document = latex_template % (colonies_content, food_content)
 
-path = os.path.join("statistics", directory)
-os.makedirs(path, exist_ok=True)
+    directory = time.strftime("%Y-%m-%d_%H-%M-%S")
 
-with open(os.path.join(path, "statistics.tex"), 'w') as output_file:
-    output_file.write(latex_document)
+    path = os.path.join("statistics", directory)
+    os.makedirs(path, exist_ok=True)
 
-os.chdir("statistics\\" + directory)
+    with open(os.path.join(path, "statistics.tex"), 'w') as output_file:
+        output_file.write(latex_document)
 
-os.system(f"pdflatex statistics.tex")
+    os.chdir("statistics\\" + directory)
+
+    os.system(f"pdflatex statistics.tex")
