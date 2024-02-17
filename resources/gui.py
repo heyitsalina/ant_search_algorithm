db0719db6292d82ead9471c89e6f71ef2e087ff2
Jonah Gräfe <jonah.greafe@gmail.com>
2024-02-17 22:24:46 +0100

    changes obstacle size

--- resources/gui.py
+++ resources/gui.py
@@ -870,7 +870,7 @@ class ButtonWidget(BoxLayout):
         if sim.bounds[0] < transformed_touch[0]-50 < sim.bounds[1]-90 and sim.bounds[2]-25 < transformed_touch[1]-50 < sim.bounds[3]-90:
             with self.simulation_widget.canvas:
                 Color(0.75, 0.75, 0.75, 1)
-                obstacle = Rectangle(pos=(transformed_touch[0] - 50, transformed_touch[1] - 50), size=(100, 100))
+                obstacle = Rectangle(pos=(transformed_touch[0] - 25, transformed_touch[1] - 25), size=(50, 50))
             self.simulation_widget.unbind(on_touch_down=self.place_obstacle)
             sim.obstacles.append(obstacle)
 
