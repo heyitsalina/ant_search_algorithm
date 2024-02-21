aed0f64dbca052c3e52de0e8f1ce8b7ed25d9d4f
Jonah Gräfe <jonah.greafe@gmail.com>
2024-02-21 19:40:46 +0100

    efficiency move  changes

--- resources/ant.py
+++ resources/ant.py
@@ -26,50 +26,46 @@ class Ant:
         """
         
         self.pheromone_status = -1
-        self.coordinates = coordinates
+        self.coordinates = np.array(coordinates)
         self.amount_to_carry = amount_to_carry
         self.step_size = step_size
-        self.direction = np.array([0, 0])
+        self.direction = self.first_direction()
         self.epoch = 0
         self.ant_carries = 0
         self.search_radius = search_radius
         self.pheromone_influence = pheromone_influence
+
+    def first_direction(self):
+        theta = np.random.uniform(0, 2 * np.pi)
+        phi = np.arccos(2 * np.random.uniform(0, 1) - 1)
+
+        x = np.sin(phi) * np.cos(theta)
+        y = np.sin(phi) * np.sin(theta)
+
+        return np.array([x, y]) * self.step_size
+
         
     def switch_pheromone(self):
         """Switches the pheromone status of the ant."""
         self.pheromone_status *= -1
     
-    def move(self, angle_offset = 0, pheromone_direction=None):
-
-        position = np.array(self.coordinates)
+    def move(self, angle_offset=0, pheromone_direction=None):
         
-        if self.epoch == 0:
-            #Randomly set initial direction in coordinates
-            theta = np.random.uniform(0, 2 * np.pi)
-            z = np.random.uniform(-1, 1)
-            
-            x = np.sqrt(1 - z**2) * np.cos(theta)
-            y = np.sqrt(1 - z**2) * np.sin(theta)
-            
-            #Set direction vector        
-            self.direction = np.array([x, y]) * self.step_size
+        if angle_offset == 0:
+            angle_offset = np.random.uniform(-np.pi / 4, np.pi / 4)
 
-        else:
-            
-            if angle_offset == 0:
-                #Apply a random rotation to the existing direction
-                angle_offset = np.random.uniform(-np.pi/4, np.pi/4)
-                
-            rotation_matrix = np.array([[np.cos(angle_offset), -np.sin(angle_offset)],
-                                    [np.sin(angle_offset), np.cos(angle_offset)]])
-            
-            #Rotate and normalize the direction vector
-            self.direction = np.dot(rotation_matrix, self.direction)
-            if pheromone_direction is not None:
-                self.direction += pheromone_direction * self.pheromone_influence
-            self.direction = self.direction / np.linalg.norm(self.direction) * self.step_size
-                
-        future_position = position + self.direction
+        rotation_matrix = np.array([[np.cos(angle_offset), -np.sin(angle_offset)],
+                                        [np.sin(angle_offset), np.cos(angle_offset)]])
+
+        self.direction = np.dot(rotation_matrix, self.direction)
+
+        if pheromone_direction is not None:
+            self.direction += pheromone_direction * self.pheromone_influence
+
+        self.direction /= np.linalg.norm(self.direction)
+        self.direction *= self.step_size
+
+        future_position = self.coordinates + self.direction
         self.epoch += 1
         return tuple(future_position)
      
