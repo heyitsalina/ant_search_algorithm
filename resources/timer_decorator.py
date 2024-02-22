ec8b3d426575a54654e5b4b93b0d4438dc308687
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-02-22 13:11:20 +0100

    added TimeTracker Class and some other changes

--- resources/timer_decorator.py
+++ resources/timer_decorator.py
@@ -1,8 +1,11 @@
 import time
 
+class TimeTracker:
+    execution_times = {}
+
 def time_this(func):
     """
-    A decorator that measures and prints the execution time of a function.
+    A decorator that measures the execution time of a function and adds it to the total execution time.
 
     Parameters:
     func (function): The function to be decorated.
@@ -14,6 +17,16 @@ def time_this(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        print(f"Execution time of {func.__name__}: {end_time - start_time:.4f} seconds")
+        elapsed_time = end_time - start_time
+
+        if func.__name__ not in TimeTracker.execution_times:
+            TimeTracker.execution_times[func.__name__] = 0.0
+        TimeTracker.execution_times[func.__name__] += elapsed_time
+
         return result
     return wrapper
+
+def print_execution_times():
+    for func_name, total_time in TimeTracker.execution_times.items():
+        print(f"Total execution time of {func_name}: {total_time:.4f} seconds")
+    TimeTracker.execution_times.clear()
