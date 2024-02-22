833b61cd63e11bfc2c1d67d851dbde36bc322889
heyitsalina <alina.weidemann04@gmail.com>
2024-02-23 00:02:25 +0100

    docstrings pheromone

--- resources/pheromone.py
+++ resources/pheromone.py
@@ -6,25 +6,24 @@ class Pheromone:
         """
         Manages pheromone information in a tensor within a simulated environment.
         The tensor represents the pheromone strength at different positions within the simulation area.
+        ----------
 
         Args:
             grid_shape (Tuple[int, int]): A tuple representing the height and width of the simulated environment.
+        ----------
 
         Attributes:
             pheromones (numpy.ndarray): A 3D numpy array of dimensions (Depth, Height, Width), storing the pheromone strength at each visited position(int).
                                         The depth represents different pheromone matrices ('coming from colony' = -1 | 'coming from food' = 1).
+        ----------
 
         Methods:
             leave_pheromone():
                 Leaves a pheromone from each ant at every movement, adding it to the corresponding location in the tensor.
 
-            get_pheromone_level():
-                Determines the pheromone level at a specific location in the tensor.
-
             reduce_pheromone(reducing_factor: float, zero_threshold: float):
                 Reduces the pheromone strength in the tensor after each epoch.
         """
-
         self.pheromone_array = np.zeros((2, grid_shape[0], grid_shape[1]))
 
     @time_this
@@ -32,11 +31,13 @@ class Pheromone:
         """
         Leaves pheromone at a given position based on the pheromone status. This method is typically
         called when an ant moves, to mark its trail with pheromones.
+        ----------
 
         Args:
             pos (Tuple[int, int]): The (x, y) coordinates in the grid where the pheromone is to be placed.
             pheromone_status (int): The status of the pheromone to be left, which determines the depth 
                                     in the tensor where the pheromone is placed.
+        ----------
 
         Returns:
             None. This method modifies the internal state of the pheromone tensor.
@@ -55,11 +56,13 @@ class Pheromone:
         """
         Reduces the pheromone level by a reduction factor every epoch.
         By the multiplication these will be reduced weighted by their amount, higher amount of pheromones results in higher reduction.
+        ------------
 
         Args:
             reduction_factor (float): The factor by which the pheromones should be reduced each epoch.
             zero_threshold (float): The value at which the pheromone value is so low that it should be considered 0.
-
+        ------------
+        
         Returns:
             None. This method modifies the internal state of the pheromone tensor.
         
