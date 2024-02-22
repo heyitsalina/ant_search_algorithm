99acd02026282ce64e3a419ebb996fddb567abe2
heyitsalina <alina.weidemann04@gmail.com>
2024-02-23 00:01:06 +0100

    docstrings obstacle

--- resources/obstacle.py
+++ resources/obstacle.py
@@ -1,4 +1,14 @@
 class Obstacle:
+    """
+    Initializes an obstacle object within the search space.
+    -------
+
+    Args:
+    coordinates (tuple):
+        The (x, y) coordinates of the obstacle in the search space.
+    size (tuple):
+        The size of the obstacle in the search space. Defaults to (50, 50).
+    """
     def __init__(self, coordinates, size=(50, 50)):
         self.coordinates = coordinates
         self.size = size
