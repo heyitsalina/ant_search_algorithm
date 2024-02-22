f55aff2ddcfcfd180d49f53a277fbef12b45e20d
heyitsalina <alina.weidemann04@gmail.com>
2024-02-22 23:45:53 +0100

    docstrings ant

--- resources/ant.py
+++ resources/ant.py
@@ -24,6 +24,27 @@ class Ant:
             The current direction vector of the ant.
         epoch (int):
             Represents the current epoch or step in the movement of the ant.
+        ---------
+
+        Methods:
+        def first_direction():
+            Generates a random direction on the 2D plane using spherical coordinates.
+        def move():
+            Moves the agent based on its current direction and optional pheromone influence.
+        def switch_pheromone():
+            Switches the pheromone status of the ant.
+        def move(): 
+            Moves the ant in the search space.
+        def is_near_target():
+            Determines if an ant is within a specified radius of a food or colony source.
+        def try_carry_food ():
+            Determine if the ant can pick up food from a specified source.
+        def carry_food()
+            Have the ant pick up food from the specified source and update its status.
+        def try_drop_food():
+            Determine if the ant can drop food at its colony.
+        def drop_food():
+            Have the ant drop food at its colony and update its status.
         """
         
         self.pheromone_status = -1
@@ -37,6 +58,13 @@ class Ant:
         self.pheromone_influence = pheromone_influence
 
     def first_direction(self):
+        """
+        Generates a random direction on the 2D plane using spherical coordinates.
+        ----------
+
+        Returns:
+        numpy array: A 2D vector representing the random direction scaled by the step size.
+        """
         theta = np.random.uniform(0, 2 * np.pi)
         phi = np.arccos(2 * np.random.uniform(0, 1) - 1)
 
@@ -46,12 +74,24 @@ class Ant:
         return np.array([x, y]) * self.step_size
 
     def switch_pheromone(self):
-        """Switches the pheromone status of the ant."""
+        """
+        Switches the pheromone status of the ant.
+        """
         self.pheromone_status *= -1
         
     @time_this 
     def move(self, pheromone_direction=None):
+        """
+        Moves the ant based on its current direction and optional pheromone influence.
+        ---------
 
+        Args:
+            pheromone_direction (numpy array, optional): An additional directional influence based on pheromones.
+        ---------
+
+        Returns:
+            tuple: The future position of the ant after moving.
+        """
         position = np.array(self.coordinates)
         
         angle_offset = np.random.uniform(-np.pi / 4, np.pi / 4)
@@ -77,9 +117,9 @@ class Ant:
          
     @time_this
     def is_near_target(self, target_position, center_offset=45, radius=20):
-
         """
         Determines if an ant is within a specified radius of a food or colony source.
+        ----------
 
         Parameters:
         target_position (tuple):
@@ -88,10 +128,11 @@ class Ant:
             The offset value to adjust the center of the target. Defaults to 45 => offset of source of food.
         radius (int):
             The radius of the circular area around the target. Defaults to 20 => radius of source of food.
+        ----------
 
         Returns:
-        tuple: The current (x, y) coordinates of the ant if it is within the specified radius; 
-        otherwise, returns None.
+            tuple: The current (x, y) coordinates of the ant if it is within the specified radius; 
+                   otherwise, returns None.
         """
 
         target_center_x, target_center_y = target_position[0] + center_offset, target_position[1] + center_offset
@@ -106,9 +147,11 @@ class Ant:
     def try_carry_food(self, food):
         """
         Determine if the ant can pick up food from a specified source.
-        
+        ----------
+
         Args:
             food (Food): The food source to potentially pick up food from.
+        ----------
 
         Returns:
             bool: True if the ant can carry food, False otherwise.
@@ -119,6 +162,7 @@ class Ant:
     def carry_food(self, food):
         """
         Have the ant pick up food from the specified source and update its status.
+        -----------
 
         Args:
             food (Food): The food source to pick up food from.
@@ -132,9 +176,11 @@ class Ant:
     def try_drop_food(self, colony):
         """
         Determine if the ant can drop food at its colony.
+        ---------
 
         Args:
             colony (Colony): The colony to potentially drop food at.
+        ---------
 
         Returns:
             bool: True if the ant can drop food, False otherwise.
@@ -146,6 +192,7 @@ class Ant:
     def drop_food(self, colony):
         """
         Have the ant drop food at its colony and update its status.
+        ----------
 
         Args:
             colony (Colony): The colony to drop food at.
