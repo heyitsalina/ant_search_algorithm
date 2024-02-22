319ceadc8df7811f789348eeb6f96257a19b4ef1
heyitsalina <alina.weidemann04@gmail.com>
2024-02-22 23:57:16 +0100

    docstrings colony

--- resources/colony.py
+++ resources/colony.py
@@ -7,9 +7,10 @@ class Colony:
     def __init__(self, grid_pheromone_shape, amount, size, coordinates, color, show_pheromone=False):
         """
         This class represents the Ant-colony.
+        --------
 
         Args:
-        grid_pheromone_shape(tuple):
+        grid_pheromone_shape (tuple):
             number of rows and columns of pheromone array
         amount (float):
             amount of Ants per colony
@@ -17,7 +18,26 @@ class Colony:
             size of the colonys nest
         coordinates (tuple):
             The (x,y) coordinates of the ants nest
+        colors (str):
+            The color representation of the colony.
+        show_pheromone (bool, optional):
+            Whether to display pheromone trails. Defaults to False.
+        ----------
+
+        Attributes:
+        pheromone (Pheromone):
+            The pheromone object and its grid shape associated with the colony.
+        ants (list):
+            List of ants belonging to the colony.
+        food_counter (int):
+            Counter to track the amount of food collected by the colony.
+        -----------
+
+        Methods:
+        def add_ants():
+            Add ants to the colony.
         """
+
         self.pheromone = Pheromone(grid_shape=grid_pheromone_shape)
         self.amount = amount
         self.size = size
@@ -31,6 +51,20 @@ class Colony:
         
     @time_this
     def add_ants(self, amount_to_carry=1, step_size=3, search_radius=1, pheromone_influence=0.01):
+        """
+        Add ants to the colony.
+        --------
+
+        Args:
+        amount_to_carry (float):
+            The maximum amount that each ant can carry. Defaults to 1.
+        step_size (float):
+            The distance covered by ants in each step during movement. Defaults to 3.
+        ---------
+
+        Returns:
+            None.
+        """
 
         for _ in range(self.amount):
             self.ants.append(Ant(coordinates=(self.coordinates[0]+50, self.coordinates[1]+50),
@@ -38,4 +72,3 @@ class Colony:
                                  step_size=step_size, 
                                  search_radius=search_radius,
                                  pheromone_influence=pheromone_influence))
-    
