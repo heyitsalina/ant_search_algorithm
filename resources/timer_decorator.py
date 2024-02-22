18667d36858c9b98755800cd0a3cdd0055e69f41
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-02-22 12:53:32 +0100

    created timer_decorator.py

---
+++ resources/timer_decorator.py
@@ -0,0 +1,13 @@
+import time
+
+def time_this(func):
+    """
+    A decorator that measures and prints the execution time of a function.
+
+    Parameters:
+    func (function): The function to be decorated.
+
+    Returns:
+    function: The wrapper function.
+    """
+    pass
